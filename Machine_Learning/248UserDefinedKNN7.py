e0789fba6de126ee0f21744a5a9bb425b7b876a0
Om Mahale <connect.ommahale123@gmail.com>
2026-08-14 19:08:15 +0530

    Develop User Defined KNN

---
+++ Machine_Learning/248UserDefinedKNN7.py
@@ -0,0 +1,65 @@
+import math
+import numpy as np
+
+def MarvellousEucDistance(P1,P2):
+
+    Ans = math.sqrt((P1["X"] - P2["X"])**2 + (P1["Y"] - P2["Y"])**2)
+    return Ans
+
+def MarvellousKNNClassifier():
+    Border = "-"*30
+
+    Data = [
+        {"point" :  "A" ,"X" : 1, "Y" : 2,"label" : "Red"},
+        {"point" :  "B" ,"X" : 2, "Y" : 3,"label" : "Red"},
+        {"point" :  "C" ,"X" : 3, "Y" : 1,"label" :"Blue"},
+        {"point" :  "D" ,"X" : 5, "Y" : 6,"label": "Blue"}
+    ]
+    print(Border)
+    print("Marvellous KNN Classifier")
+    print(Border)
+
+    for i in Data:
+        print(i)
+    print(Border)
+
+    new_point = {"X" : 3, "Y" : 3}
+
+    print("Distances of all Point : ")
+    print(Border)
+    for d in Data:
+        d["Distance"] = (MarvellousEucDistance(d,new_point))
+
+    for d in Data:
+        print(d)
+
+    print(Border)
+
+    sorted_data = sorted(Data,key=lambda item : item["Distance"])
+
+    print(Border)
+    print("Sorted Data : ")
+    print(Border)
+
+    for d in sorted_data:
+        print(d)
+    print(Border)
+
+    k = 3
+
+    nearest = sorted_data[:k]
+    print(Border)
+    print("Nearest 3 members are : ")
+    print(Border)
+
+    for d in nearest:
+        print(d)
+
+    print(Border)
+
+    
+def main():
+    MarvellousKNNClassifier()
+
+if __name__ == "__main__":
+    main()
---
+++ Machine_Learning/251UserDefinedKNN10.py
@@ -0,0 +1,92 @@
+import math
+import numpy as np
+
+def MarvellousEucDistance(P1,P2):
+
+    Ans = math.sqrt((P1["X"] - P2["X"])**2 + (P1["Y"] - P2["Y"])**2)
+    return Ans
+
+def MarvellousKNNClassifier(k = 3):     #Change
+    Border = "-"*30
+
+    Data = [
+        {"point" :  "A" ,"X" : 1, "Y" : 2,"label" : "Red"},
+        {"point" :  "B" ,"X" : 2, "Y" : 3,"label" : "Red"},
+        {"point" :  "C" ,"X" : 3, "Y" : 1,"label" :"Blue"},
+        {"point" :  "D" ,"X" : 5, "Y" : 6,"label": "Blue"},
+        {"point" :  "E" ,"X" : 6, "Y" : 6,"label": "Blue"},
+        {"point" :  "F" ,"X" : 3, "Y" : 4,"label": "Red"},
+        {"point" :  "G" ,"X" : 3, "Y" : 2,"label": "Red"}
+    ]
+    print(Border)
+    print("Marvellous KNN Classifier")
+    print(Border)
+
+    for i in Data:
+        print(i)
+    print(Border)
+
+    new_point = {"X" : 3, "Y" : 3}
+
+    print("Distances of all Point : ")
+    print(Border)
+    for d in Data:
+        d["Distance"] = (MarvellousEucDistance(d,new_point))
+
+    for d in Data:
+        print(d)
+
+    print(Border)
+
+    sorted_data = sorted(Data,key=lambda item : item["Distance"])
+
+    print(Border)
+    print("Sorted Data : ")
+    print(Border)
+
+    for d in sorted_data:
+        print(d)
+    print(Border)
+
+    nearest = sorted_data[:k]
+    print(Border)
+    print("Nearest 3 members are : ")
+    print(Border)
+
+    for d in nearest:
+        print(d)
+
+    print(Border)
+
+    #Voting
+    votes = {}
+
+    for neighbours in nearest:
+        label = neighbours["label"]
+        votes[label] = votes.get(label,0) + 1
+
+    print(Border)
+    print("Voting Result is : ")
+    print(Border)
+
+    for d in votes:
+        print("Name : ",d,"Number of Votes : ",votes[d])
+
+    print(Border)
+
+    iMax = 0
+    Name = ""
+
+    for d in votes:
+        if(votes[d] > iMax):
+            iMax = votes[d]
+            Name = d
+
+    print("Final Prediction is : ",Name)
+
+    
+def main():
+    MarvellousKNNClassifier(5)
+
+if __name__ == "__main__":
+    main()
