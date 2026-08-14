b41ec67e159b9fe6168f94da3ff2f5f84e06fd39
Om Mahale <connect.ommahale123@gmail.com>
2026-08-15 02:09:19 +0530

    Added Wine Predictor Case Study using KNN Classifier

---
+++ Machine_Learning/255WinePredictorKNN4.py
@@ -0,0 +1,77 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score,confusion_matrix
+from sklearn.preprocessing import StandardScaler
+
+
+def MarvellousClassifier(DataPath):
+    Border = "-"*40
+
+    print(Border)
+    print("Step 1 : Load the Dataset")
+    print(Border)
+
+    df = pd.read_csv(DataPath)
+
+    print(Border)
+    print("Some entries from dataset : ")
+    print(df.head())
+    print(Border)
+
+    #Step 2 : Clean the Dataset
+
+    print(Border)
+    print("Step 2 : CLean the Dataset")
+    print(Border)
+
+    df.dropna(inplace=True)
+
+    print("Shape of Dataset : ",df.shape)
+    print("Total Records : ",df.shape[0])
+    print("Total Columns : ",df.shape[1])
+
+    print(Border)
+
+    #Step 3 : Seperate Independent and Dependent Variable
+
+    print(Border)
+    print("Step 3 : Seperate Independent and Dependent Variable")
+    print(Border)
+
+    X = df.drop(columns=["Class"])
+    Y = df["Class"]
+
+    print("SHape of X : ",X.shape)
+    print("Shape of Y : ",Y.shape)
+
+    print(Border)
+    print("Input Columns : ",X.columns.tolist())
+    print("Output Columns : Class")
+    print(Border)
+
+    #Step 4 - Split the Data for Training and Testing Purpose
+    print(Border)
+    print("Step 4 : Split the Data for Training and Testing Purpose")
+    print(Border)
+
+    X_train,X_test,Y_train,Y_test = train_test_split(X,Y,test_size=0.2,random_state=42,stratify=Y)
+    print(Border)
+
+    print("Details of Training and Testing Data : ")
+
+    print("SHape of X_train : ",X_train.shape)
+    print("SHape of X_test : ",X_test.shape)
+
+    print("SHape of Y_train : ",Y_train.shape)
+    print("SHape of Y_test : ",Y_test.shape)
+    print(Border)
+
+
+def main():
+    MarvellousClassifier("WinePredictor.csv")
+
+if __name__ =="__main__":
+    main()
---
+++ Machine_Learning/257WinePredictorKNN6.py
@@ -0,0 +1,119 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score,confusion_matrix
+from sklearn.preprocessing import StandardScaler
+
+
+def MarvellousClassifier(DataPath):
+    Border = "-"*40
+
+    print(Border)
+    print("Step 1 : Load the Dataset")
+    print(Border)
+
+    df = pd.read_csv(DataPath)
+
+    print(Border)
+    print("Some entries from dataset : ")
+    print(df.head())
+    print(Border)
+
+    #Step 2 : Clean the Dataset
+
+    print(Border)
+    print("Step 2 : CLean the Dataset")
+    print(Border)
+
+    df.dropna(inplace=True)
+
+    print("Shape of Dataset : ",df.shape)
+    print("Total Records : ",df.shape[0])
+    print("Total Columns : ",df.shape[1])
+
+    print(Border)
+
+    #Step 3 : Seperate Independent and Dependent Variable
+
+    print(Border)
+    print("Step 3 : Seperate Independent and Dependent Variable")
+    print(Border)
+
+    X = df.drop(columns=["Class"])
+    Y = df["Class"]
+
+    print("SHape of X : ",X.shape)
+    print("Shape of Y : ",Y.shape)
+
+    print(Border)
+    print("Input Columns : ",X.columns.tolist())
+    print("Output Columns : Class")
+    print(Border)
+
+    #Step 4 - Split the Data for Training and Testing Purpose
+    print(Border)
+    print("Step 4 : Split the Data for Training and Testing Purpose")
+    print(Border)
+
+    X_train,X_test,Y_train,Y_test = train_test_split(X,Y,test_size=0.5,random_state=42,stratify=Y)
+    print(Border)
+
+    print("Details of Training and Testing Data : ")
+
+    print("SHape of X_train : ",X_train.shape)
+    print("SHape of X_test : ",X_test.shape)
+
+    print("SHape of Y_train : ",Y_train.shape)
+    print("SHape of Y_test : ",Y_test.shape)
+    print(Border)
+
+    #Step 5 : Feature Scaling
+
+    print(Border)
+    print("Step 5 : Feature Scaling")
+    print(Border)
+
+    scalar = StandardScaler()
+    X_train_scaled = scalar.fit_transform(X_train)
+    X_test_scaled = scalar.fit_transform(X_test)
+
+    print("Feature Scaling Done")
+    print(Border)
+
+    #Step 6 : Build the model
+
+    print(Border)
+    print("Step 6 : Build the model")
+    print(Border)
+
+    model = KNeighborsClassifier(n_neighbors=9)
+    print("Classification Model is Created")
+
+    #Step 7 : Train the model
+
+    print(Border)
+    print("Step 7 : Train the model")
+    print(Border)
+
+    model = model.fit(X_train_scaled,Y_train)
+    print("Model Trained Successfully")
+
+    #Step 8 : Test the Model
+
+    print(Border)
+    print("Step 8 : Test the Model")
+    print(Border)
+
+    Y_pred = model.predict(X_test_scaled)
+
+    accuracy = accuracy_score(Y_test,Y_pred)
+
+    print("Accuracy of model is : ",accuracy*100)
+
+def main():
+    MarvellousClassifier("WinePredictor.csv")
+
+if __name__ =="__main__":
+    main()
