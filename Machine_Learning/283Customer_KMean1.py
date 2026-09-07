ce511a89cc6a1666ad03865e8b1f4d6c31908f74
Om Mahale <connect.ommahale123@gmail.com>
2026-09-07 20:37:05 +0530

    Added KMean Algorithms

---
+++ Machine_Learning/283Customer_KMean1.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from sklearn.preprocessing import StandardScaler
+from sklearn.cluster import KMeans
+
+def main():
+
+    #Step 1 : Load the Data
+
+    df = pd.read_csv("Mall_Customers.csv")
+    print("Data Set Loaded print with values")
+    print(df.head())
+    print("Missing Value : ")
+    print(df.isnull().sum())
+
+if __name__ == "__main__":
+    main()
---
+++ Machine_Learning/285Customer_KMean3.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from sklearn.preprocessing import StandardScaler
+from sklearn.cluster import KMeans
+
+def main():
+
+    #Step 1 : Load the Data
+    df = pd.read_csv("Mall_Customers.csv")
+    print("Data Set Loaded print with values")
+    print(df.head())
+    print("Missing Value : ")
+    print(df.isnull().sum())
+
+    #Step 2 : Feature Selection
+    X = df[["AnnualIncome","SpendingScore"]]
+    print("Selected Features : ")
+    print(X.head())
+
+    #Step 3 : Scale the Data
+    scalar = StandardScaler()
+
+    X_scaled = scalar.fit_transform(X)
+    print("Scaled Data is : ")
+    print(X_scaled[:5])
+
+if __name__ == "__main__":
+    main()
